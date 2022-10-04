8204700ba52abde439a3a692529d7eb5ef1be9ee
rdhlily <lily@redistrictingdatahub.org>
2022-10-03 20:00:16 -0400

    with updated baf

--- national_baf_cd_population_request/cd2022Pop2020/main.py
+++ national_baf_cd_population_request/cd2022Pop2020/main.py
@@ -11,10 +11,12 @@ files:
 import pandas as pd
 import geopandas as gp
 import os
+#test
 
 #first need to download and unzip baf since it is csv
 baf = pd.read_csv('./national_baf/national_baf.csv')
 cd = gp.read_file(f'zip+s3://data.redistrictingdatahub.org/web_ready_stage/NATIONAL/national_cong_2022.zip')
+cd_pop_adj = pd.read_csv('./adjusted_districts_pop.csv')
 
 state_abrvs = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
            'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
@@ -37,87 +39,76 @@ def national_pl():
 def baf_pl_merge():
     global natpl
     natpl = national_pl()
-    natpl['GEOID20'] = natpl['GEOID20'].str.zfill(16)
-    baf['GEOID20'] = baf['GEOID20'].str.zfill(16)
-    baf_pl = baf.merge(natpl, on='GEOID20', how='outer', indicator=False)
-
-    return baf_pl
-
-def baf_to_csv_pop(baf):
-    print("csv read in running")
-    baf_pl = baf
-    baf_pl['GEOID20'] = baf_pl['GEOID20'].astype(str).str.zfill(16)
-    for state in state_abrvs:
-        print(f"reading in {state}")
-        pl = gp.read_file(f'zip+s3://data.redistrictingdatahub.org/web_ready_stage/PL2020/shp/{state.lower()}'+'_pl2020_b.zip')[
-            ['GEOID20', 'P0010001', 'geometry']]
-        #assert set(pl['GEOID20']) - set(baf['GEOID20']) == set()
-        pl['GEOID20'] = pl['GEOID20'].astype(str).str.zfill(16)
-        baf_pl = pl.merge(baf_pl, on='GEOID20', how='outer', indicator=False)
-        #break
-    #assert baf.shape[0] == baf_pl.shape[0]
-    assert ~baf_pl['GEOID20'].isnull().any()
+    natpl['GEOID20'] = natpl['GEOID20'].astype(str).str.zfill(16)
+    baf['GEOID20'] = baf['GEOID20'].astype(str).str.zfill(16)
+    baf_pl = natpl.merge(baf, on='GEOID20', how='outer', indicator=False)
 
     return baf_pl
 
 
-def baf_to_pop(baf):
-    print("baf_to_pop() running")
-    baf_pl = baf
-    baf_pl['GEOID20'] = baf_pl['GEOID20'].astype(str).str.zfill(16)
-    for state in state_abrvs:
-        print(f"reading in {state}")
-        pl = gp.read_file(f'zip+s3://data.redistrictingdatahub.org/web_ready_stage/PL2020/shp/{state.lower()}'+'_pl2020_b.zip')[
-            ['GEOID20', 'P0010001', 'geometry']]
-        #assert set(pl['GEOID20']) - set(baf['GEOID20']) == set()
-        pl['GEOID20'] = pl['GEOID20'].astype(str).str.zfill(16)
-        baf_pl = pl.merge(baf_pl, on='GEOID20', how='outer', indicator=False)
-        #break
-    #assert baf.shape[0] == baf_pl.shape[0]
-    assert ~baf_pl['GEOID20'].isnull().any()
-
-    return baf_pl
-
-
-def baf_groupby_cd(baf):
-    print("groupby running")
-    baf_pl = baf_to_pop(baf)
+def baf_groupby_cd():
+    print("baf_groupby_cd running")
+    baf_pl = baf_pl_merge()
     baf_pl['CD_ID'] = baf_pl['STATEAB'].astype(str) + '-' + baf_pl['CONG'].astype(str)
     baf_pl_sum = baf_pl.groupby(['CD_ID']).sum()
 
     return baf_pl_sum
 
-# TODO : add in join s3://data.redistrictingdatahub.org/web_ready_stage/requested_data/districts_adjusted_pop.zip
 
-def cd_to_baf_pl(cd, baf):
+def cd_to_baf_pl():
     print("cd_to_baf_pl() running")
-    cd['CD_ID'] = cd['STATE'].astype(str) + '-' + cd['DISTRICT'].astype(str)
-    baf_pl_sum = baf_groupby_cd(baf)
-    cd_pop = baf_pl_sum.merge(cd, on="CD_ID", how = 'outer', indicator=True)
-    #assert set(cd_pop['_merge']) == {'both'}
-    final = cd_pop[['DISTRICT', 'STATE', 'P0010001', 'geometry']]
+    cd['CD_ID'] = cd['STATE'].astype(str) + '-' + cd['DISTRICT'].astype(str).str.zfill(3)
+    baf_pl_sum = baf_groupby_cd()
+    baf_pl_sum = baf_pl_sum.reset_index()
+    cd_pop_geo = cd.merge(baf_pl_sum[['CD_ID', 'P0010001']], on="CD_ID", how='outer', indicator=True)
 
-    return final
+    return cd_pop_geo
+
+
+def cd_pl_to_adj_pop():
+    print("cd_pl_to_adj_pop() running")
+    adj = pd.read_csv("./adjusted_districts_pop.csv")
+    cd_adj = adj[adj['Level'] == 'CONG']
+    cd_adj['CD_ID'] = cd_adj['State'] + '-' + cd_adj['ID'].str.zfill(3)
+    cd_pop_geo = cd_to_baf_pl()
+    cd_adj_pop_geo = cd_pop_geo.merge(cd_adj, on='CD_ID', how='outer')
+
+    return cd_adj_pop_geo
+
+
 
 # TODO: groupby state to check total pop for state
 # TODO: check value counts of districts total (435)
 # TODO: If not assigned it will aggregate to a district with no assignment , remove unassigned districts
-def export_cd_pop(cd, baf):
-    print("cd pop export running")
-    formatted_cd_pop = cd_to_baf_pl(cd, baf)
-    export = formatted_cd_pop[~formatted_cd_pop['geometry'].isna()]
+def export_cd_pop():
+    print("export_cd_pop() running")
+    cd_pop_geo = cd_to_baf_pl()
+    #assert set(cd_pop_geo['_merge']) == {'both'}
+    cd_pop_geo = cd_pop_geo[['DISTRICT', 'STATE', 'P0010001', 'geometry']]
     try:
         os.mkdir('./national_cd_pop_2022/')
     except:
         print("folder already exists, moving on")
-    export.to_file('./national_cd_pop_2022/national_cd_pop_2022.shp')
+    cd_pop_geo.to_file('./national_cd_pop_2022/national_cd_pop_2022.shp')
 
     print("cd pop file exported!")
 
 
+def export_cd_adj():
+    print("export_cd_adj() running")
+    cd_adj = cd_pl_to_adj_pop()
+    try:
+        os.mkdir('./national_cd_pop_adj_2022/')
+    except:
+        print("folder already exists, moving on")
+    cd_adj.to_file('./national_cd_pop_adj_2022/national_cd_pop_adj_2022.shp')
+
+    print("cd adj pop file exported!")
+
+
 def export_baf_pop(baf):
     print("baf pop export running")
-    formatted_baf_pop = baf_to_pop(baf)#[['GEOID20', 'STATEAB', 'CONG', 'P0010001']]#, 'geometry']]
+    formatted_baf_pop = baf_pl_merge()[['GEOID20', 'STATEAB', 'CONG', 'P0010001', 'geometry']]
     try:
         os.mkdir('./national_baf_pop_2022/')
     except:
